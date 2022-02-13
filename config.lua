d491a787c62e5a4486359080d62a16ec2627b0d2
Hagen <hagen@hyena.gay>
2022-02-13 13:07:20 +0000

    refactor: update typings

--- config.lua
+++ config.lua
@@ -1,5 +1,8 @@
 Config = {
-    StatuspageURL = 'myapp.statuspage.io'
-    -- Can also be an array of status pages!
-    -- StatuspageURL = {'myapp.statuspage.io', 'status.cfx.re'}
+    --- The URL to the statuspage you would like to keep track off.
+    --- Can also be an array of statuspages.
+    --- @type string|table<any, string>|nil
+    --- @example StatuspageURL = { 'myapp.statuspage.io', 'status.cfx.re' }
+    --- @example StatuspageURL = 'myapp.statuspage.io'
+    StatuspageURL = nil,
 }
